1d8532b347a33ed40bda26a67346063482a76064
Sudhir <Sudhir@localhost>
2016-09-06 08:50:25 -0400

    Modified package name of Expression Tree code

---
+++ AdvancedDS/src/com/test/eTree/ExpressionTreeHandler.java
@@ -0,0 +1,25 @@
+package com.test.eTree;
+
+public class ExpressionTreeHandler {
+
+	public static void main(String[] args) {
+		ExpressionTree eTree = new ExpressionTree();
+		String postfix = "45+3/7*42/-";
+		
+		eTree.buildTree(postfix);
+		eTree.display();
+		
+		System.out.println("Prefix Expression:");
+		eTree.prefix();
+		
+		System.out.println("Postfix Expression:");
+		eTree.postfix();
+		
+		System.out.println("Parenthesized Infix Expression:");
+		eTree.parenthesizedInfix();
+		
+		System.out.print("value of Tree: "+eTree.evaluate());
+
+	}
+
+}
---
+++ AdvancedDS/src/com/test/eTree/Node.java
@@ -0,0 +1,14 @@
+package com.test.eTree;
+
+public class Node {
+	Node leftChild;
+	char info; //this values would either be an operator or operand
+	Node rightChild;
+	
+	public Node(char c)
+	{
+		info  =c;
+		leftChild = null;
+		rightChild = null;
+	}
+}
